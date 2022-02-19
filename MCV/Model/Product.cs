3efce5e125042d3c06323d3673f5b65d47c5900b
Poorva Joshi <56001271+poorvajoshi1305@users.noreply.github.com>
2022-02-19 18:36:29 +0530

    Add files via upload

---
+++ MCV/Model/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DOTNETEXAMproduct.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Plase Fill ProductName")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Plase Fill ProductName")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
+        public decimal Rate { get; set; }
+
+        [Required(ErrorMessage = "Plase Fill ProductName")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Plase Fill ProductName")]
+        public string CategoryName { get; set; }
+    }
+}
